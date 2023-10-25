e2be9bc1e9f9833ab38546e641a1dc71ebe8596e
bazylskyi <andrew.bazilskiy@gmail.com>
2023-10-25 16:36:13 -0400

    Bellman and Dijkstra pros and cons

---
+++ algorithms/search/bellman.mjs
@@ -0,0 +1,11 @@
+// Pros:
+// Very effective at solving the shortest path
+// over Dijkstra's algorithm because
+// can accommodate negative weight
+
+// Cons:
+// Can take a long time to run in terms of complexity
+// It's not the most efficient algorithm
+// The worst case O(n^2)
+
+// TODO
---
+++ algorithms/search/dijkstra.mjs
@@ -0,0 +1,7 @@
+// Pros:
+// A little bit faster and a little bit more efficient than Bellman's algo
+
+// Cons:
+// can't accommodate negative weights between nodes
+
+// TODO
