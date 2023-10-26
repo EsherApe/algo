0e5b49953a2127b31b1d352ed187e529dc9579ee
bazylskyi <andrew.bazilskiy@gmail.com>
2023-10-26 12:59:39 -0400

    dynamic programming, enhanced fibonacci

--- tasks/recursion.js
+++ tasks/recursion.js
@@ -22,7 +22,7 @@ const factorialRecursive = (num) => {
     return num * factorialRecursive(num - 1)
 } // O(n)
 
-// Fibonaci
+// Fibonacci
 const fibonacciIterative = (n) => {
     // first two steps return n
     if (n < 2) {
@@ -55,10 +55,42 @@ const fibonacciIterativeArray = (n) => {
  
 } // O(n)
 
+let countSlow = 0
 const fibonacciRecursive = (n) => {
+    countSlow++
+
     if (n < 2) {
         return n
     }
 
     return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
 } // O(2^n)
+
+let countFast = 0
+const fibonacciMemoized = () => {
+    const cache = {}
+
+    const fib = (n) => {
+        countFast++
+
+        if (!cache[n]) {
+            if (n < 2) {
+                return n
+            }
+    
+            cache[n] = fib(n-1) + fib(n-2)
+            return cache[n]
+        }
+
+        return cache[n]
+    }
+
+    return fib
+} // O(n)
+
+const fasterFib = fibonacciMemoized()
+
+console.log(fibonacciRecursive(20))
+console.log(fasterFib(20))
+console.log(`slow ${countSlow} calculations`)
+console.log(`fast ${countFast} calculations`)
