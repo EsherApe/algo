e31b13e9806fdaeb62f2b6ccbd20e541011b7e9d
Andrii Bazylskyi <andrew.bazilskiy@gmail.com>
2023-11-03 14:38:35 -0400

    longest common prefix

---
+++ exercises/longestCommonPrefix/index.js
@@ -0,0 +1,14 @@
+const longestCommonPrefix = (strs) => {
+    let i = 1;
+    let prefix = strs[0];
+    while(i < strs.length){
+        if(!strs[i].startsWith(prefix)){
+            prefix = prefix.slice(0, -1)
+        }else{
+            i++
+        }
+    }
+    return prefix
+}
+
+longestCommonPrefix(["flower","flow","flight"])
