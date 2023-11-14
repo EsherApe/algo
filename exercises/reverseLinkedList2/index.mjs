08f89abea86ac2f874253c30e9e8bfd2f6502635
Andrii Bazylskyi <andrew.bazilskiy@gmail.com>
2023-11-14 15:19:14 -0500

    reverse linked list 2

---
+++ exercises/reverseLinkedList2/index.mjs
@@ -0,0 +1,37 @@
+import { LinkedList } from '../../dataStructures/LinkedList.mjs'
+
+const reverseLinkedList2 = (head, left, right) => {
+    let currentPos = 1, currentNode = head
+    let start = head
+  
+    while(currentPos < left) {
+        start = currentNode
+        currentNode = currentNode.next
+        currentPos++
+    }
+  
+    let newList = null, tail = currentNode
+  
+    while(currentPos >= left && currentPos <= right) {
+        const next = currentNode.next
+        currentNode.next = newList
+        newList = currentNode
+        currentNode = next
+        currentPos++
+    }
+  
+    start.next = newList
+    tail.next = currentNode
+  
+    if(left > 1) {
+        return head
+    } else {
+        return newList
+    }
+} // time: O(n) space: O(1)
+
+const myLinkedList = new LinkedList(1)
+myLinkedList.append(2)
+myLinkedList.append(3)
+
+reverseLinkedList2(myLinkedList.head, 1,3)
